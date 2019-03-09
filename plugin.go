827c7e2d86ac86ebc22ba4f3e90a3b3279c0391f
unknown <cedric@DESKTOP-PRRRUI0.finbel.intra>
2019-03-09 07:50:31 +0100

    working hub

--- plugin.go
+++ plugin.go
@@ -16,33 +16,14 @@ import (
 
 const (
 	//DefaultRegistry is the default value for the registry
-	DefaultRegistry = "https://hub.docker.com"
-	//DefaultRegex is the default match for tags to clean
-	DefaultRegex = "^[A-Fa-f0-9]+$"
-	//DefaultMin is the default value for the number of tags/images to keep
-	DefaultMin = 3
-	//DefaultMax is the maxium age of images in the registry
-	DefaultMax = "15d"
+	DefaultRegistry = "https://cloud.docker.com"
 	//HubPageSize docker hub page size
 	HubPageSize = 100
 )
 
 type (
-	//Repo repository informations
-	Repo struct {
-		FullName string
-		Owner    string
-	}
-
-	//Author author informations
-	Author struct {
-		Name   string
-		Email  string
-		Avatar string
-	}
-
-	//Config plugin configuration informations
-	Config struct {
+	//Plugin plugin data
+	Plugin struct {
 		Username string
 		Password string
 		Repo     string
@@ -50,130 +31,124 @@ type (
 		Regex    string
 		Min      int
 		Max      time.Duration
-	}
-
-	//Plugin plugin data
-	Plugin struct {
-		Repo   Repo
-		Config Config
+		Verbose  bool
+		DryRun   bool
 	}
 )
 
-//Init set default values to config
-func (c *Config) Init() error {
-	if len(c.Registry) == 0 {
-		c.Registry = DefaultRegistry
-	}
-	if len(c.Regex) == 0 {
-		c.Regex = DefaultRegex
-	}
-	if c.Min == 0 {
-		c.Min = DefaultMin
-	}
-	if c.Max.Seconds() == 0 {
-		d, err := time.ParseDuration(DefaultMax)
-		if err != nil {
-			return fmt.Errorf("invalud default duration provided (%s)", DefaultMax)
-		}
-		c.Max = d
-	}
-	return nil
-}
-
 //Check the config values
-func (c *Config) Check() error {
+func (p *Plugin) Check() error {
 	// direct validation
-	if len(c.Username) == 0 {
+	if len(p.Username) == 0 {
 		return fmt.Errorf("empty username provided")
 	}
-	if len(c.Password) == 0 {
+	if len(p.Password) == 0 {
 		return fmt.Errorf("empty password provided")
 	}
-	if len(c.Registry) == 0 {
+	if len(p.Registry) == 0 {
 		return fmt.Errorf("no registry provided")
 	}
-	if len(c.Repo) == 0 {
+	if len(p.Repo) == 0 {
 		return fmt.Errorf("no repository provided")
 	}
-	if len(c.Regex) == 0 {
+	if len(p.Regex) == 0 {
 		return fmt.Errorf("no regex match provided")
 	}
-	if c.Min == 0 {
+	if p.Min == 0 {
 		return fmt.Errorf("no minimum ammount of images/tags to keep")
 	}
-	if c.Max.Seconds() == 0 {
+	if p.Max.Seconds() == 0 {
 		return fmt.Errorf("no maximum age provided")
 	}
 	// complex validations
 	// check registry
-	_, err := url.Parse(c.Registry)
+	_, err := url.Parse(p.Registry)
 	if err != nil {
-		return fmt.Errorf("registry is not in url format (%s)", c.Registry)
+		return fmt.Errorf("registry is not in url format (%s)", p.Registry)
 	}
 	// check Regex
-	_, err = regexp.Compile(c.Regex)
+	_, err = regexp.Compile(p.Regex)
 	if err != nil {
-		return fmt.Errorf("invalid regex provided (%s)", c.Regex)
+		return fmt.Errorf("invalid regex provided (%s)", p.Regex)
 	}
 	// avoid full match
-	if c.Regex == ".*" || c.Regex == "^.*$" {
-		return fmt.Errorf("regex would match everything (%s)", c.Regex)
+	if p.Regex == ".*" || p.Regex == "^.*$" {
+		return fmt.Errorf("regex would match everything (%s)", p.Regex)
 	}
 	return nil
 }
 
 //Exec executes the registry-cleanup plugin
 func (p Plugin) Exec() error {
-	// set default paramters
-	err := p.Config.Init()
-	if err != nil {
-		return err
-	}
 	// check paramaters
-	err = p.Config.Check()
+	err := p.Check()
 	if err != nil {
 		return err
 	}
-	if p.Config.Registry == DefaultRegistry {
+	// if default registry use docker hub api
+	if p.Registry == DefaultRegistry {
 		return p.ExecHub()
 	}
+	// else use registry api
 	return p.ExecRegistry()
 }
 
 //ExecHub executes the registry-cleanup plugin on the docker hub
 func (p Plugin) ExecHub() error {
 	// get the base url
-	baseurl := fmt.Sprintf("%s/v2/", p.Config.Registry)
+	baseurl := fmt.Sprintf("%s/v2/", p.Registry)
 	// initialize rest client
 	r := rest.NewClient()
 	// get a token
-	data, err := r.Post(fmt.Sprintf("%s/users/login/", baseurl), map[string]string{"username": p.Config.Username, "password": p.Config.Password})
+	data, err := r.Post(fmt.Sprintf("%susers/login/", baseurl), map[string]string{"username": p.Username, "password": p.Password})
 	if err != nil {
+		if p.Verbose {
+			fmt.Println(err)
+		}
 		return fmt.Errorf("could not get token")
 	}
 	var token hub.Token
 	err = json.Unmarshal(data, &token)
 	if err != nil {
+		if p.Verbose {
+			fmt.Println(err)
+		}
 		return fmt.Errorf("cannot read token from response")
 	}
-	r.Headers["Authorization"] = token.Token
+	if p.Verbose {
+		fmt.Printf("authenticated with %s\n", p.Username)
+	}
+	r.Headers["Authorization"] = fmt.Sprintf("Bearer %s", token.Token)
 	// get the tag list
 	var tags []hub.Tag
-	re := regexp.MustCompile(p.Config.Regex)
+	re := regexp.MustCompile(p.Regex)
 	var tagpage hub.Tags
-	tagpage.Next = fmt.Sprintf("%s/repositories/%s/tag/?page_size=%d&page=%d", baseurl, p.Config.Repo, HubPageSize, 1)
+	tagpage.Next = fmt.Sprintf("%srepositories/%s/tags/?page_size=%d&page=%d", baseurl, p.Repo, HubPageSize, 1)
 	// loop trought the result pages
 	for len(tagpage.Next) > 0 {
 		data, err = r.Get(tagpage.Next)
+		if err != nil {
+			if p.Verbose {
+				fmt.Println(err)
+			}
+			return fmt.Errorf("cannot get tag page")
+		}
+		tagpage = hub.Tags{}
 		err = json.Unmarshal(data, &tagpage)
 		if err != nil {
+			if p.Verbose {
+				fmt.Println(err)
+			}
 			return fmt.Errorf("cannot read tag page response")
 		}
 		for _, tag := range tagpage.Results {
-			if !re.MatchString(tag.Name) {
+			if !re.MatchString(tag.Name) || tag.Name == "latest" {
 				continue
 			}
 			tags = append(tags, tag)
+		}
+		if p.Verbose {
+			fmt.Printf("found %d tags/images\n", len(tags))
 		}
 	}
 	// order tags per date (newer to older)
@@ -181,12 +156,12 @@ func (p Plugin) ExecHub() error {
 		return tags[i].LastUpdated.After(tags[j].LastUpdated)
 	})
 	// parse the tags in reverse order to decice which to delete
-	treshold := time.Now().Add(-p.Config.Max)
+	treshold := time.Now().Add(-p.Max)
 	var wg sync.WaitGroup
 	deleting := false
 	for i := len(tags) - 1; i >= 0; i-- {
 		// stop if reached the minimum limit
-		if i <= p.Config.Min-1 {
+		if i <= p.Min-1 {
 			break
 		}
 		// delete if older than treshold
@@ -197,15 +172,21 @@ func (p Plugin) ExecHub() error {
 				deleting = true
 			}
 			// send the delete request async
-			go func() {
-				_, err := r.Delete(fmt.Sprintf("%s/repositories/%s/tag/%s", baseurl, p.Config.Repo, tags[i].Name))
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "%s updated at '%s' error deleting", tags[i].Name, tags[i].LastUpdated.Format(time.RFC822))
-				} else {
-					fmt.Printf("%s updated at '%s' deleted", tags[i].Name, tags[i].LastUpdated.Format(time.RFC822))
+			go func(tag hub.Tag) {
+				defer wg.Done()
+				if p.DryRun {
+					fmt.Printf("%s:%s (%s) would be deleted\n", p.Repo, tag.Name, tag.LastUpdated.Format(time.RFC822))
+					return
 				}
-				wg.Done()
-			}()
+				_, err := r.Delete(fmt.Sprintf("%srepositories/%s/tags/%s/", baseurl, p.Repo, tag.Name))
+				if err != nil {
+					if p.Verbose {
+						fmt.Println(err)
+					}
+					fmt.Fprintf(os.Stderr, "%s:%s (%s) error deleting\n", p.Repo, tag.Name, tag.LastUpdated.Format(time.RFC822))
+				}
+				fmt.Printf("%s:%s (%s) deleted\n", p.Repo, tag.Name, tag.LastUpdated.Format(time.RFC822))
+			}(tags[i])
 		}
 	}
 	// if deleting woit for the results
--- registry-cleanup.go
+++ registry-cleanup.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/urfave/cli"
@@ -17,80 +18,70 @@ func main() {
 	app.Action = run
 	app.Version = version
 	app.Flags = []cli.Flag{
-
-		//
-		// repo args
-		//
-
 		cli.StringFlag{
-			Name:   "repo.fullname",
-			Usage:  "repository full name",
-			EnvVar: "DRONE_REPO",
-		},
-		cli.StringFlag{
-			Name:   "repo.owner",
-			Usage:  "repository owner",
-			EnvVar: "DRONE_REPO_OWNER",
-		},
-
-		//
-		// Parameters
-		//
-		cli.StringFlag{
-			Name:   "config.username",
+			Name:   "username",
 			Usage:  "Docker username",
 			EnvVar: "PLUGIN_USERNAME,DRONE_REPO_OWNER",
 		},
 		cli.StringFlag{
-			Name:   "config.password",
+			Name:   "password",
 			Usage:  "Docker password",
 			EnvVar: "PLUGIN_PASSWORD",
 		},
 		cli.StringFlag{
-			Name:   "config.repo",
+			Name:   "repo",
 			Usage:  "Repository to target",
 			EnvVar: "PLUGIN_REPO,DRONE_REPO",
 		},
 		cli.StringFlag{
-			Name:   "config.registry",
+			Name:   "registry",
+			Value:  DefaultRegistry,
 			Usage:  "Registry to target",
 			EnvVar: "PLUGIN_REGISTRY",
 		},
 		cli.StringFlag{
-			Name:   "config.regex",
+			Name:   "regex",
+			Value:  "^[0-9A-Fa-f]+$",
 			Usage:  "Clean Tags that match regex",
 			EnvVar: "PLUGIN_REGEX",
 		},
 		cli.IntFlag{
-			Name:   "config.min",
+			Name:   "min",
+			Value:  3,
 			Usage:  "Minimum number of tags/images to keep",
 			EnvVar: "PLUGIN_MIN",
 		},
 		cli.DurationFlag{
-			Name:   "config.max",
+			Name:   "max",
+			Value:  360 * time.Hour,
 			Usage:  "Maximum age of tags/images",
 			EnvVar: "PLUGIN_MAX",
 		},
-	}
+		cli.BoolFlag{
+			Name:   "verbose",
+			Usage:  "Show verbose information",
+			EnvVar: "PLUGIN_VERBOSE",
+		},
+		cli.BoolFlag{
+			Name:   "dryrun",
+			Usage:  "Dry run",
+			EnvVar: "PLUGIN_DRYRUN",
+		}}
 
 	app.Run(os.Args)
 }
 
 func run(c *cli.Context) {
 	plugin := Plugin{
-		Repo: Repo{
-			FullName: c.String("repo.fullname"),
-			Owner:    c.String("repo.owner"),
-		},
-		Config: Config{
-			Username: c.String("config.username"),
-			Password: c.String("config.password"),
-			Repo:     c.String("config.repo"),
-			Registry: c.String("config.registry"),
-			Regex:    c.String("config.regex"),
-			Min:      c.Int("config.min"),
-			Max:      c.Duration("config.max"),
-		},
+		Username: c.String("username"),
+		Password: c.String("password"),
+		Repo:     c.String("repo"),
+		Registry: c.String("registry"),
+		Regex:    c.String("regex"),
+		Min:      c.Int("min"),
+		Max:      c.Duration("max"),
+		Verbose:  c.Bool("verbose"),
+		DryRun:   c.Bool("dryrun"),
 	}
 
 	if err := plugin.Exec(); err != nil {
